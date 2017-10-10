581f5b96b02632ff1eea9cc26b505872f2043347
Jan Cepelak <cepelak.honza@gmail.com>
2017-10-10 20:12:30 +0200

    Second Seminar

---
+++ Second Solution/Priklad 01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priklad_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pro zadaný intervalu <a,b> kde a,b  vypiště součet čísel, které jsou sudé a dělitelné 5.
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = a; i <= b; i++) // lze pouzit i mmodulo 10
+            {
+                if (i % 2 == 0 && i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine("Soucet cisel je: {0}", sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Second Solution/Priklad 02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Priklad_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Pro zadaný intervalu <a,b> kde a,b  vypiště součet násobků Pi (přibližné násobky).
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            double sum = 0;
+            //while (sum < b)
+            //{
+            //    sum += pi;
+            //    if (sum < b)
+            //    {
+            //        celkem += sum;
+            //    }
+
+            //}
+            double nejnizsiNasobekIntervalu = Math.Ceiling(nejnizsiNasobekIntervalu = a / Math.PI) * Math.PI;
+            double nasobekPI = nejnizsiNasobekIntervalu;
+            while (nasobekPI <= b)
+            {
+                sum += nasobekPI;
+                nasobekPI += Math.PI;
+            }
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
